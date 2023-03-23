4340b68c5b377fe42b88df04bc307a2c3d1f7064
MonPham <demondhunter93@gmail.com>
2023-03-23 14:46:23 +0700

    Update start-prover-relayer.sh

--- script/start-prover-relayer.sh
+++ script/start-prover-relayer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -eou pipefail
-
+: ${RPCD:=zkevm-chain-prover-rpcd}
 if [ "$ENABLE_PROVER" == "true" ]; then
     if [ ! -f "./wait" ];then
         wget https://github.com/ufoscout/docker-compose-wait/releases/download/2.9.0/wait
@@ -17,7 +17,7 @@ if [ "$ENABLE_PROVER" == "true" ]; then
         --l2.http http://l2_execution_engine:${PORT_L2_EXECTION_ENGINE_HTTP} \
         --taikoL1 ${TAIKO_L1_ADDRESS} \
         --taikoL2 ${TAIKO_L2_ADDRESS} \
-        --zkevmRpcdEndpoint http://zkevm-chain-prover-rpcd:${PORT_ZKEVM_CHAIN_PROVER_RPCD} \
+        --zkevmRpcdEndpoint http://${RPCD}:${PORT_ZKEVM_CHAIN_PROVER_RPCD} \
         --zkevmRpcdParamsPath /data \
         --l1.proverPrivKey ${L1_PROVER_PRIVATE_KEY} \
         --maxConcurrentProvingJobs 1
